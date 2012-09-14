68c83355150581a9a3a28d587cc4bc0325f63192
Jun He <junnhe@gmail.com>
2012-09-13 20:12:24 -0700

    add run id support for for pull-minavgmax.sh

--- scripts/pull-minavgmax.sh
+++ scripts/pull-minavgmax.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
-# This is a shell script that pull out the min, avg, max value of
-# fs-test output.
+flags="-nodb -io mpi -strided 1 -barriers aopen -barrier aclose"
+flags1="-nodb -io mpi -strided 1 -barriers aopen -barrier aclose -deletefile"
 
 testid=envtest_pattern_8-8
 #testid=envtest_2.2.1_8-8
@@ -13,8 +12,9 @@ extract_one () {
     op=$3
     filename=$4
     rtype=$5
+    runid=$6
 
-    str=`sh -c "grep \"$keyword\"  $filename.log.$op$rtype"`
+    str=`sh -c "grep \"$keyword\"  $filename.log.$op$rtype$runid"`
     #echo $str
     val=`echo $str | cut -d ' ' -f $pos`
     if [ -z $val ] 
@@ -28,66 +28,68 @@ extract_one () {
 
 extract_all_write () {
     filename=$1
-    
-    open_min=`extract_one "File_Write_Open Time" 9 "write" $filename`
+    runid=".$2"
+
+    open_min=`extract_one "File_Write_Open Time" 9 "write" $filename "" $runid`
     echo $open_min
-    open_avg=`extract_one "File_Write_Open Time" 12 "write" $filename`
+    open_avg=`extract_one "File_Write_Open Time" 12 "write" $filename "" $runid`
     echo $open_avg
-    open_max=`extract_one "File_Write_Open Time" 13 "write" $filename`
+    open_max=`extract_one "File_Write_Open Time" 13 "write" $filename "" $runid`
     echo $open_max
 
-    band_min=`extract_one "Write Bandwidth" 9 "write" $filename`
+    band_min=`extract_one "Write Bandwidth" 9 "write" $filename "" $runid`
     echo $band_min
-    band_avg=`extract_one "Write Bandwidth" 12 "write" $filename`
+    band_avg=`extract_one "Write Bandwidth" 12 "write" $filename "" $runid`
     echo $band_avg
-    band_max=`extract_one "Write Bandwidth" 13 "write" $filename`
+    band_max=`extract_one "Write Bandwidth" 13 "write" $filename "" $runid`
     echo $band_avg
 
-    eband_min=`extract_one "Effective Bandwidth" 9 "write" $filename`
+    eband_min=`extract_one "Effective Bandwidth" 9 "write" $filename "" $runid`
     echo $eband_min
-    eband_avg=`extract_one "Effective Bandwidth" 12 "write" $filename`
+    eband_avg=`extract_one "Effective Bandwidth" 12 "write" $filename "" $runid`
     echo $eband_avg
-    eband_max=`extract_one "Effective Bandwidth" 13 "write" $filename`
+    eband_max=`extract_one "Effective Bandwidth" 13 "write" $filename "" $runid`
     echo $eband_max
 
-    close_min=`extract_one "Write_File_Close_Wait Time" 9 "write" $filename`
+    close_min=`extract_one "Write_File_Close_Wait Time" 9 "write" $filename "" $runid`
     echo $close_min
-    close_avg=`extract_one "Write_File_Close_Wait Time" 12 "write" $filename`
+    close_avg=`extract_one "Write_File_Close_Wait Time" 12 "write" $filename "" $runid`
     echo $close_avg
-    close_max=`extract_one "Write_File_Close_Wait Time" 13 "write" $filename`
+    close_max=`extract_one "Write_File_Close_Wait Time" 13 "write" $filename "" $runid`
     echo $close_max
 }
 
 extract_all_read () {
     filename=$1
     rtype=".$2"
+    runid=".$3"
     
-    open_min=`extract_one "File_Read_Open Time" 9 "read" $filename $rtype`
+    open_min=`extract_one "File_Read_Open Time" 9 "read" $filename $rtype $runid`
     echo $open_min
-    open_avg=`extract_one "File_Read_Open Time" 12 "read" $filename $rtype`
+    open_avg=`extract_one "File_Read_Open Time" 12 "read" $filename $rtype $runid`
     echo $open_avg
-    open_max=`extract_one "File_Read_Open Time" 13 "read" $filename $rtype`
+    open_max=`extract_one "File_Read_Open Time" 13 "read" $filename $rtype $runid`
     echo $open_max
 
-    band_min=`extract_one "Read Bandwidth" 9 "read" $filename $rtype`
+    band_min=`extract_one "Read Bandwidth" 9 "read" $filename $rtype $runid`
     echo $band_min
-    band_avg=`extract_one "Read Bandwidth" 12 "read" $filename $rtype`
+    band_avg=`extract_one "Read Bandwidth" 12 "read" $filename $rtype $runid`
     echo $band_avg
-    band_max=`extract_one "Read Bandwidth" 13 "read" $filename $rtype`
+    band_max=`extract_one "Read Bandwidth" 13 "read" $filename $rtype $runid`
     echo $band_avg
 
-    eband_min=`extract_one "Effective Bandwidth" 9 "read" $filename $rtype`
+    eband_min=`extract_one "Effective Bandwidth" 9 "read" $filename $rtype $runid`
     echo $eband_min
-    eband_avg=`extract_one "Effective Bandwidth" 12 "read" $filename $rtype`
+    eband_avg=`extract_one "Effective Bandwidth" 12 "read" $filename $rtype $runid`
     echo $eband_avg
-    eband_max=`extract_one "Effective Bandwidth" 13 "read" $filename $rtype`
+    eband_max=`extract_one "Effective Bandwidth" 13 "read" $filename $rtype $runid`
     echo $eband_max
 
-    close_min=`extract_one "Read_File_Close_Wait Time" 9 "read" $filename $rtype`
+    close_min=`extract_one "Read_File_Close_Wait Time" 9 "read" $filename $rtype $runid`
     echo $close_min
-    close_avg=`extract_one "Read_File_Close_Wait Time" 12 "read" $filename $rtype`
+    close_avg=`extract_one "Read_File_Close_Wait Time" 12 "read" $filename $rtype $runid`
     echo $close_avg
-    close_max=`extract_one "Read_File_Close_Wait Time" 13 "read" $filename $rtype`
+    close_max=`extract_one "Read_File_Close_Wait Time" 13 "read" $filename $rtype $runid`
     echo $close_max
 }
 
@@ -111,20 +113,27 @@ echo np nobj objsize testid \
      erband.min.archive erband.avg.archive erband.max.archive \
      rclose.min.archive rclose.avg.archive rclose.max.archive 
 
+
 objsize=4096
-for np in 8 64 256 
-#for np in 1 2 4 8 32 64 128 256 512 
+nplist="32 64 128 256 512"
+nobjlist="8 32 128 512 2048 8192 32768 131072 524288 1048576"
+idlist="1 2 3 4 5"
+
+for np in $nplist 
 do
-    for nobj in 1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 
+    for nobj in $nobjlist 
     do
-        filename=$np.$nobj.$objsize.$testid
-        
-        writeperf=`extract_all_write $filename`
-        readuniform=`extract_all_read $filename uniform`
-        readnonuniform=`extract_all_read $filename nonuniform`
-        #readarchive=`extract_all_read $filename archive`
-        echo $np $nobj $objsize $testid \
-             $writeperf $readuniform $readnonuniform $readarchive
+        for id in $idlist
+        do
+            filename=$np.$nobj.$objsize.$testid
+            
+            writeperf=`extract_all_write $filename $id`
+            readuniform=`extract_all_read $filename uniform $id`
+            readnonuniform=`extract_all_read $filename nonuniform $id`
+            readarchive=`extract_all_read $filename archive $id`
+            echo $np $nobj $objsize $testid \
+                 $writeperf $readuniform $readnonuniform $readarchive $id
+        done
     done
 done
 
