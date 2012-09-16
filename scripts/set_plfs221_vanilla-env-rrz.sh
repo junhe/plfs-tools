d906dd0b64063ba04b79927b556ad702e6208d51
Jun He <jun@lanl.gov>
2012-09-16 16:15:35 -0600

    correct wrong path

--- scripts/set_plfs221_vanilla-env-rrz.sh
+++ scripts/set_plfs221_vanilla-env-rrz.sh
@@ -3,7 +3,7 @@ unset LD_LIBRARY_PATH
 export PATH=/usr/lib64/qt-3.3/bin:/usr/local/bin:/bin:/usr/bin:/usr/X11R6/bin:/opt/MOAB/bin:/opt/PBS/bin:/opt/MOAB/bin:/opt/PBS/bin:/opt/MOAB/bin:/opt/PBS/bin:/opt/MOAB/bin:/opt/PBS/bin
 
 # Add PLFS 2.2.1 Vanilla
-export PATH=/users/jun/installs/plfs-2.2.1-vanilla/sbin:/user/jun/installs/plfs-2.2.1-vanilla/bin:$PATH
+export PATH=/users/jun/installs/plfs-2.2.1-vanilla/sbin:/users/jun/installs/plfs-2.2.1-vanilla/bin:$PATH
 export LD_LIBRARY_PATH=/users/jun/installs/plfs-2.2.1-vanilla/lib
 
 # Add OpenMPI 1.4.3 Vanilla
