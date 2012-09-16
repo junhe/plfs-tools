39c34819414069f3fbb7e8e7acf003659e7e6b2f
Jun He <jun@lanl.gov>
2012-09-16 16:16:14 -0600

    added set pagoda

---
+++ scripts/set_pagoda_env.sh
@@ -0,0 +1,16 @@
+unset LD_LIBRARY_PATH
+
+export PATH=/usr/lib64/qt-3.3/bin:/usr/local/bin:/bin:/usr/bin:/usr/X11R6/bin:/opt/MOAB/bin:/opt/PBS/bin:/opt/MOAB/bin:/opt/PBS/bin:/opt/MOAB/bin:/opt/PBS/bin:/opt/MOAB/bin:/opt/PBS/bin
+
+# Add PLFS 2.2.1 Vanilla
+export PATH=/users/jun/installs/pagoda-0.6/bin:$PATH
+export LD_LIBRARY_PATH=/users/jun/installs/pagoda-0.6/lib
+
+# Add Pnetcdf
+export PATH=/users/jun/installs/pnetcdf-1.3.0/bin:$PATH
+export LD_LIBRARY_PATH=/users/jun/installs/pnetcdf-1.3.0/lib:$LD_LIBRARY_PATH
+
+
+# Add OpenMPI 1.4.3 Vanilla
+export PATH=/users/jun/installs/openmpi-1.4.3-vanilla/bin:$PATH
+export LD_LIBRARY_PATH=/users/jun/installs/openmpi-1.4.3-vanilla/lib:$LD_LIBRARY_PATH
